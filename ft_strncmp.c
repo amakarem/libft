7c55f1c0f6a7a3c0c71c5fe7156c3214ad736cea
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-08 21:12:17 +0200

    norm

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,20 +6,21 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 20:29:41 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/08 20:51:13 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/08 21:11:52 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	unsigned int	i;
 
 	i = 0;
 	while (str1[i] != '\0' && str2[i] != '\0' && i <= n)
 	{
-		if (str1[i] != str2[i]) {
+		if (str1[i] != str2[i])
+		{
 			return (str1[i] - str2[i]);
 		}
 		i++;
