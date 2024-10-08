ab7a3fc9f258145cbff4ba947b032abedf9d63cf
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-08 17:41:11 +0200

    norm

--- libft.h
+++ libft.h
@@ -6,15 +6,17 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:51:31 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/07 17:35:31 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:17:00 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
-void	ft_putchar(char c);
-void    ft_putstr(char *s);
-void	ft_putnbr(int n);
+#include <unistd.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 size_t	ft_strlen(char *s);
-
-//size_t	strlen(const char *s);
-
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+void	ft_putnbr(int n);
