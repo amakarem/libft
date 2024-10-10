7404c5aa6d5d48330756d652982dfa4cc34e6df1
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-10 22:17:08 +0200

    fix bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 17:31:03 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/10 18:33:36 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/10 22:13:43 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_bzero(void *str, size_t n)
 {
-	size_t			i;
-	unsigned char	*ptr;
+	size_t	i;
+	char	*ptr;
 
 	i = 0;
-	ptr = (unsigned char *)str;
+	ptr = (char *)str;
 	while (i < n)
 	{
-		ptr[0] = 0;
+		ptr[i] = 0;
 		i++;
 	}
-	return (NULL);
+	return ((void *) ptr);
 }
