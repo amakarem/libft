c823c98ddba165a8a2461ca78f2eba1fd288c7ba
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-10 21:33:08 +0200

    fix strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 18:35:18 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/08 19:51:35 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/10 21:32:45 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,15 @@ char	*ft_strchr(const char *str, int c)
 {
 	while (*str != '\0')
 	{
-		if (*str == c)
+		if (*str == (char)c)
 		{
 			return ((char *)str);
 		}
 		str++;
+	}
+	if (*str == (char)c)
+	{
+		return ((char *)str);
 	}
 	return ((char *) NULL);
 }
