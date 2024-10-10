194599b410cf284abc9db6a1f46ffbe032d5b55d
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-10 21:24:53 +0200

    fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 20:29:41 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/08 21:11:52 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/10 21:24:14 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	while (str1[i] != '\0' && str2[i] != '\0' && i <= n)
+	while (str1[i] != '\0' && str2[i] != '\0' && i < n)
 	{
-		if (str1[i] != str2[i])
+		if ((unsigned char)str1[i] != (unsigned char)str2[i])
 		{
-			return (str1[i] - str2[i]);
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		}
 		i++;
 	}
-	if (i != n)
+	if (i < n)
 	{
-		return (str1[i] - str2[i]);
+		return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 	}
 	return (0);
 }
