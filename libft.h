7d533fa601bead313bb2fb7ae0048d03110a29f8
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-16 19:21:23 +0200

    putendl_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:51:31 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/16 19:07:58 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/16 19:16:01 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(const char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 #endif
