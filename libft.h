b73f797ba3139fb762c3fa8e05ccc1262a7734a1
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-16 21:42:04 +0200

    istlast

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:51:31 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/16 21:19:28 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/16 21:30:16 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,5 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
