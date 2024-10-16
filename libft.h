9652b215e976f0dabdfa42057053947825ca35b8
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-16 21:29:14 +0200

    lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:51:31 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/16 21:09:39 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/16 21:19:28 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,5 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 #endif
