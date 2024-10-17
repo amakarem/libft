9e97f9896224b2ebddda904df57db5b23826ad1d
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-10-17 18:33:36 +0200

    use pointr to fix memory issue

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 22:52:57 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/16 23:46:19 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/10/17 18:33:03 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*obj;
+	void	*ptr;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new = NULL;
 	while (lst)
 	{
-		obj = ft_lstnew(f(lst->content));
+		ptr = f(lst->content);
+		obj = ft_lstnew(ptr);
 		if (obj == NULL)
 		{
+			del(ptr);
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
