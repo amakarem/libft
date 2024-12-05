42e9985917153f8cc366a71c7e2aea73151265c0
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-12-05 18:03:37 +0100

    optmization

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,37 +6,34 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:05:39 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/10/13 16:25:57 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:02:52 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*ptr;
-	size_t	len;
 	int		i;
+	int		j;
 
-	len = ft_strlen(s1) + ft_strlen(s2);
-	ptr = malloc(sizeof(char) * len + 1);
+	if (!s2)
+		return (NULL);
+	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (ptr == NULL)
 		return (NULL);
-	len = 0;
+	j = 0;
 	i = 0;
-	while (s1[i] != '\0')
+	if (s1)
 	{
-		ptr[len] = s1[i];
-		len++;
-		i++;
+		while (s1[i] != '\0')
+			ptr[j++] = s1[i++];
+		free(s1);
 	}
 	i = 0;
 	while (s2[i] != '\0')
-	{
-		ptr[len] = s2[i];
-		len++;
-		i++;
-	}
-	ptr[len] = '\0';
+		ptr[j++] = s2[i++];
+	ptr[j] = '\0';
 	return (ptr);
 }
