d80ec85987e64fe58665e4a53db7e96638152a1c
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2024-12-05 18:35:49 +0100

    optmize

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:05:39 by aelaaser          #+#    #+#             */
-/*   Updated: 2024/12/05 18:02:52 by aelaaser         ###   ########.fr       */
+/*   Updated: 2024/12/05 18:14:51 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
 	int		i;
@@ -25,14 +25,10 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (NULL);
 	j = 0;
 	i = 0;
-	if (s1)
-	{
-		while (s1[i] != '\0')
-			ptr[j++] = s1[i++];
-		free(s1);
-	}
+	while (s1 && s1[i] != '\0')
+		ptr[j++] = s1[i++];
 	i = 0;
-	while (s2[i] != '\0')
+	while (s2 && s2[i] != '\0')
 		ptr[j++] = s2[i++];
 	ptr[j] = '\0';
 	return (ptr);
